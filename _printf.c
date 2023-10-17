6e141e6a6c1615e47b82012e2765af869c007ef1
uwen-godwin <uwengodwin001@gmail.com>
2023-10-16 20:31:57 -0700

    updated

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Custom printf function
+ * @format: A format string.
+ * Return: The number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+	int k, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (k = 0; format && format[k] != '\0'; k++)
+	{
+		if (format[k] != '%')
+		{
+			buffer[buff_ind++] = format[k];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &k);
+			width = get_width(format, &k, list);
+			precision = get_precision(format, &k, list);
+			size = get_size(format, &k);
+			++k;
+			printed = handle_print(format, &k, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;	
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists.
+ * @buffer: Array of characters.
+ * @buff_ind: Index representing the length of the data in the buffer.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}

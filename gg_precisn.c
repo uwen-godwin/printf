85ddb331ef3fb225de1ba614af6e1bbd6ed1c97d
uwen-godwin <uwengodwin001@gmail.com>
2023-10-18 06:02:09 -0700

    updated

--- gg_precisn.c
+++ gg_precisn.c
@@ -13,14 +13,14 @@ int get_precision(const char *format, int *i, va_list list)
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[cuur_i] != '.')
+	if (format[curr_i] != '.')
 	{
 		return (precision);
 	}
 	precision = 0;
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[cuur_i]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';

8b1fb2a4ffbf0e458e90ec02b39fab5aae783e85
uwen-godwin <uwengodwin001@gmail.com>
2023-10-18 05:30:04 -0700

    updated

--- main.h
+++ main.h
@@ -7,13 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -30,8 +31,15 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associat
+ */
 typedef struct fmt fmt_t;
 
+/****************** FUNCTIONS ******************/
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
@@ -73,7 +81,7 @@ int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 int print_rot13string(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);

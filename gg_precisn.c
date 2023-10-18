8227fd2dc2091e8ff6f55a9fb9c3d9d064998b11
uwen-godwin <uwengodwin001@gmail.com>
2023-10-18 06:29:37 -0700

    updated

--- gg_precisn.c
+++ gg_precisn.c
@@ -27,15 +27,13 @@ int get_precision(const char *format, int *i, va_list list)
 		}
 		else if (format[curr_i] == '*')
 		{
-			curr_i++;
 			precision = va_arg(list, int);
+			curr_i++;
 			break;
 		}
 		else
 			break;
 	}
-	if (precision < 0)
-		precision = INT_MAX;
 	*i = curr_i - 1;
 	return (precision);
 }
